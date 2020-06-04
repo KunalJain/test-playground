899eb621db05399fbf3e5aefbcf8456e4edfd663
Kunal Jain <kunal@saucelabs.com>
2020-06-03 18:29:54 -0700

    Udpated test

--- tests/homepage.test.js
+++ tests/homepage.test.js
@@ -20,13 +20,36 @@
 describe('test user can pay for the room', () => {
 	test('should verify payment screen loads', async () => {
       const page = (await browser.pages())[0]
+      page.setDefaultTimeout(20000);
+
       await page.goto('https://www.hilton.com/en/hilton-honors/');
 
       await page.waitForSelector('#search-input-box');
-      await page.type('#search-input-box', "Tuscaloosa");
+      await page.type('#search-input-box', "Tuscaloosa, AL");
       await page.waitFor(10000);
-      await page.click('.sc-gqjmRU > .sc-dxZgTM > .gBdnPR');
-      await page.waitFor('.search-pagestyles__ListViewPanel-sc-1r1woml-14 > .fMSvgG');
+      await page.click('[class="sc-drlKqa sc-1rsk9xd-2 gZGhQU"]');
+      await page.waitFor('[class="search-pagestyles__ListViewPanel-sc-1r1woml-14 fMSvgG"]');
       expect(await page.title()).toBe('Hotel Search - Hilton');
+
+      await page.waitFor(10000);
+      let getFirstElement = await page.$$('[class="cta-button-primary__CTAButtonPrimary-sc-1odsntg-0 ieoKmA"]');
+      await getFirstElement[0].click();
+      await page.waitFor(10000);
+      expect(await page.title()).toBe('Available Rooms - Embassy Suites by Hilton Tuscaloosa Alabama Downtown');
+
+      getFirstElement = await page.$$('[class="MoreRatesButtonstyles__MoreRates-sc-1eyzhlz-1 kWWaFg"]');
+      await getFirstElement[0].click();
+      await page.waitFor(10000);
+      expect(await page.title()).toBe('Rates - Embassy Suites by Hilton Tuscaloosa Alabama Downtown');
+
+      await page.waitFor(20000);
+      await page.waitForSelector('[class="RateTableItemstyles__CTAwrapper-wdgpu0-7 giDwMX"]');
+      getFirstElement = await page.$$('[class="RateTableItemstyles__CTAwrapper-wdgpu0-7 giDwMX"]');
+      await getFirstElement[0].click();
+      await page.waitFor(10000);
+
+//      await page.waitForSelector('[class="FormButtonstyles__FormSubmitButton-sc-1moeqoj-0 PaymentForm__BookButton-i43r5q-2 kHSleL"]');
+//      expect(await page.title()).toBe('Rates - Embassy Suites by Hilton Tuscaloosa Alabama Downtown');
+
 	});
 });
