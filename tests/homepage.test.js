bc8ea33df372c7b7bce62610ad964980ca7cd79d
Kunal Jain <kunal@saucelabs.com>
2020-06-17 18:01:38 -0700

    Fixed tests

--- tests/homepage.test.js
+++ tests/homepage.test.js
@@ -10,10 +10,10 @@ describe('test find sign-in button', () => {
 	test('should verify user can navigate to sign-in page', async () => {
 		const page = (await browser.pages())[0]
 		await page.goto('https://www.hilton.com/en/hilton-honors/');
-	    await page.waitForSelector('.sc-hEsumM > .sc-feJyhm > .sc-caSCKo > .sc-fAjcbJ > .sc-TOsTZ:nth-child(3)')
-        await page.click('.sc-hEsumM > .sc-feJyhm > .sc-caSCKo > .sc-fAjcbJ > .sc-TOsTZ:nth-child(3)')
-        await page.navigationPromise;
+	    await page.waitForSelector('[href="https://secure3.hilton.com/en_US/hh/customer/login/index.htm"]')
+        await page.click('[href="https://secure3.hilton.com/en_US/hh/customer/login/index.htm"]')
 		expect(await page.title()).toBe('Sign In - Hilton Honors');
+        await page.waitFor(1000);
 	});
 });
 
@@ -27,8 +27,8 @@ describe('test user can pay for the room', () => {
       await page.waitForSelector('#search-input-box');
       await page.type('#search-input-box', "Tuscaloosa, AL");
       await page.waitFor(10000);
-      await page.click('[class="sc-drlKqa sc-1rsk9xd-2 gZGhQU"]');
-      await page.waitFor('[class="search-pagestyles__ListViewPanel-sc-1r1woml-14 fMSvgG"]');
+      await page.click('[data-e2e="findHotel"]');
+      await page.waitFor(10000);
       expect(await page.title()).toBe('Hotel Search - Hilton');
 
       await page.waitFor(10000);
@@ -43,14 +43,12 @@ describe('test user can pay for the room', () => {
       expect(await page.title()).toBe('Rates - Embassy Suites by Hilton Tuscaloosa Alabama Downtown');
 
       await page.waitFor(20000);
-      await page.waitForSelector('[class="RateTableItemstyles__CTAwrapper-wdgpu0-7 giDwMX"]');
-      getFirstElement = await page.$$('[class="RateTableItemstyles__CTAwrapper-wdgpu0-7 giDwMX"]');
+      await page.waitForSelector('[data-e2e="standardPriceBookCTA"]');
+      getFirstElement = await page.$$('[data-e2e="standardPriceBookCTA"]');
       await getFirstElement[0].click();
       await page.waitFor(2000);
-
-
-//      await page.waitForSelector('[class="FormButtonstyles__FormSubmitButton-sc-1moeqoj-0 PaymentForm__BookButton-i43r5q-2 kHSleL"]');
-//      expect(await page.title()).toBe('Rates - Embassy Suites by Hilton Tuscaloosa Alabama Downtown');
+      await page.waitForSelector('[class="FormButtonstyles__FormSubmitButton-sc-1moeqoj-0 PaymentForm__BookButton-i43r5q-2 kHSleL"]');
+      expect(await page.title()).toBe('Payment - Embassy Suites by Hilton Tuscaloosa Alabama Downtown');
 
 	});
 });
